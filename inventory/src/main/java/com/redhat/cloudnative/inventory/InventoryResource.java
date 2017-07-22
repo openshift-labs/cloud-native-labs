8373abadd73d855fa4938b6012cde43d4a9ab1c0
Siamak Sadeghianfar <siamaksade@gmail.com>
2017-07-22 13:21:52 +0700

    health endpoints added

--- inventory/src/main/java/com/redhat/cloudnative/inventory/InventoryResource.java
+++ inventory/src/main/java/com/redhat/cloudnative/inventory/InventoryResource.java
@@ -9,17 +9,30 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.wildfly.swarm.health.Health;
+import org.wildfly.swarm.health.HealthStatus;
+import java.util.Date;
 
-@Path("/inventory")
+@Path("/")
 @ApplicationScoped
 public class InventoryResource {
     @PersistenceContext(unitName = "MyPU")
     private EntityManager em;
 
     @GET
-    @Path("/{itemId}")
+    @Path("/inventory/{itemId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Inventory getAvailability(@PathParam("itemId") String itemId) {
         return em.find(Inventory.class, itemId);
+    }
+
+    @GET
+    @Path("/health")
+    @Health
+    public HealthStatus health() {
+        return HealthStatus
+                .named("Inventory Health")
+                .up()
+                .withAttribute("date", new Date().toString());
     }
 }
