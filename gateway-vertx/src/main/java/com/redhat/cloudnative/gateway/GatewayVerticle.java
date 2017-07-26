3920a269da18088d1156a908da4928475d2f3695
Siamak Sadeghianfar <ssadeghi@redhat.com>
2017-07-26 20:23:17 +0700

    updated gateway

--- gateway-vertx/src/main/java/com/redhat/cloudnative/gateway/GatewayVerticle.java
+++ gateway-vertx/src/main/java/com/redhat/cloudnative/gateway/GatewayVerticle.java
@@ -1,19 +1,28 @@
 package com.redhat.cloudnative.gateway;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Future;
+import io.vertx.core.*;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpClient;
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.client.HttpResponse;
-import io.vertx.ext.web.client.WebClient;
+//import io.vertx.ext.web.client.HttpResponse;
+//import io.vertx.ext.web.client.WebClient;
+//import rx.Single;
+
+import java.util.*;
 
 public class GatewayVerticle extends AbstractVerticle {
+    private HttpClient client;
+    private String inventoryUrl, catalogUrl;
+
     @Override
     public void start(Future<Void> future) {
+        inventoryUrl = config().getString("inventory.url", "http://inventory:8080");
+        catalogUrl = config().getString("catalog.url", "http://catalog:8080");
+        client = vertx.createHttpClient();
         Router router = Router.router(vertx);
 
         router.get("/health").handler(ctx -> ctx.response().end(new JsonObject().put("status", "UP").toString()));
@@ -31,17 +40,18 @@ public class GatewayVerticle extends AbstractVerticle {
     }
 
     private void products(RoutingContext rc) {
-        WebClient.create(vertx)
-                .getAbs(config().getString("endpoint.catalog", "http://catalog:8080") + "/api/catalog")
-                .send((AsyncResult<HttpResponse<Buffer>> ar) -> {
-                    if (ar.succeeded()) {
-                        rc.response().end(ar.result().bodyAsString());
-                    } else {
-                        rc.response().end(new JsonObject()
-                                        .put("service", "catalog")
-                                        .put("error", ar.cause().getMessage())
-                                        .toString());
-                    }
+        client.getAbs(catalogUrl + "/api/catalog", response -> {
+            if (response.statusCode() == 200) {
+                response.bodyHandler(productBuff -> {
+                    JsonArray products = new JsonArray(productBuff);
+                    // for each product, retrieve inventory
+                    // client.getAbs(inventoryUrl + "/api/inventory/33243", response -> {}
+
+                    rc.response().end(Json.encodePrettily(products));
                 });
+            } else {
+                rc.response().end("{\"error\": \"Catalog service failed: r" + response.statusMessage() + "\"}");
+            }
+        }).end();
     }
 }
