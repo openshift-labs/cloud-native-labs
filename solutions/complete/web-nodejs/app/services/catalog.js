104f903af4812a214327a17af44de333955e1b97
Siamak Sadeghianfar <ssadeghi@redhat.com>
2017-07-27 12:37:09 +0700

    restructured labs

---
+++ solutions/complete/web-nodejs/app/services/catalog.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module("app")
+
+.factory('catalog', ['$http', '$q', 'COOLSTORE_CONFIG', 'Auth', '$location', function($http, $q, COOLSTORE_CONFIG, $auth, $location) {
+	var factory = {}, products, baseUrl;
+
+	if ($location.protocol() === 'https') {
+		baseUrl = (COOLSTORE_CONFIG.SECURE_API_ENDPOINT.startsWith("https://") ? COOLSTORE_CONFIG.SECURE_API_ENDPOINT : "https://" + COOLSTORE_CONFIG.SECURE_API_ENDPOINT + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1")) + '/api/products';
+	} else {
+		baseUrl = (COOLSTORE_CONFIG.API_ENDPOINT.startsWith("http://") ? COOLSTORE_CONFIG.API_ENDPOINT : "http://" + COOLSTORE_CONFIG.API_ENDPOINT + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1")) + '/api/products';
+	}
+
+	factory.getProducts = function() {
+		var deferred = $q.defer();
+        if (products) {
+            deferred.resolve(products);
+        } else {
+            $http({
+                method: 'GET',
+								url: baseUrl
+            }).then(function(resp) {
+                products = resp.data;
+                deferred.resolve(resp.data);
+            }, function(err) {
+                deferred.reject(err);
+            });
+        }
+	   return deferred.promise;
+	};
+
+	return factory;
+}]);
