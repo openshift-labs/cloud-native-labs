e44db7db5bc2dad184411c27676e7364f59f7a31
Siamak Sadeghianfar <ssadeghi@redhat.com>
2017-07-28 21:17:55 +0700

    added nodejs to projects

---
+++ projects/web-nodejs/server.js
@@ -0,0 +1,47 @@
+var express = require('express'),
+    http = require('http'),
+    request = require('request'),
+    fs = require('fs'),
+    app = express(),
+    path = require("path"),
+    keycloakConfig = require('./app/keycloak.config.js'),
+    coolstoreConfig = require('./app/coolstore.config.js'),
+    Keycloak = require('keycloak-connect'),
+    cors = require('cors');
+
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    secport = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8443;
+
+// Enable CORS support
+app.use(cors());
+
+// error handling
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something bad happened!');
+});
+
+// keycloak config server
+app.get('/keycloak.json', function(req, res, next) {
+    res.json(keycloakConfig);
+});
+// coolstore config server
+app.get('/coolstore.json', function(req, res, next) {
+    res.json(coolstoreConfig);
+});
+
+app.use(express.static(path.join(__dirname, '/views')));
+app.use('/app', express.static(path.join(__dirname, '/app')));
+app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
+
+console.log("coolstore config: " + JSON.stringify(coolstoreConfig));
+console.log("keycloak config: " + JSON.stringify(keycloakConfig));
+
+
+http.createServer(app).listen(port);
+
+console.log('HTTP Server running on http://%s:%s', ip, port);
+
+module.exports = app;
