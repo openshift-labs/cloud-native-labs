e3e659cf507390fedce321b182354c3226283207
Siamak Sadeghianfar <ssadeghi@redhat.com>
2017-08-31 20:36:04 +0700

    added lab solution script for testing

---
+++ solutions/all/solve-labs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#######################
+# Lab Solution Script #
+#######################
+
+# Usage:    
+#   $ solve-labs.sh [lab-no] [lab-no] ... [lab-no]
+
+# Example:  
+#   $ solve-labs.sh 1 2 3 4 5 
+#   $ bash <(curl -sL https://raw.githubusercontent.com/openshift-roadshow/cloud-native-labs/master/solutions/all/solve-labs.sh) 1 2 3
+
+for LAB_NO in "$@"
+do
+    if [ $LAB_NO -eq 9 ]; then
+      echo
+      echo "WARNING: Lab 9 cannot be solved using this script. It requires manual steps."
+      echo
+    else
+      echo 
+      echo "########################"
+      echo " Solving Lab $LAB_NO"
+      echo "########################"
+      echo
+      bash <(curl -sL https://raw.githubusercontent.com/openshift-roadshow/cloud-native-labs/master/solutions/lab-$LAB_NO/commands.txt)
+    fi
+done
