d1705b66b2f1dcb4903a38c31739376cc0650bab
Siamak Sadeghianfar <ssadeghi@redhat.com>
2018-04-18 14:43:17 +0200

    paramterized project name in solutions

--- solutions/all/solve-labs.sh
+++ solutions/all/solve-labs.sh
@@ -11,10 +11,20 @@
 #   $ bash <(curl -sL https://raw.githubusercontent.com/openshift-labs/cloud-native-labs/ocp-3.9/solutions/all/solve-labs.sh) 1 2 3
 
 GITHUB_ACCOUNT=${GITHUB_ACCOUNT:-openshift-labs}
+PROJECT_NAME=${PROJECT_NAME:-coolstore}
+
+echo "##########################################################"
+echo " Using Project Name: ${PROJECT_NAME}"
+echo " You can set the project name by 'export PROJECT_NAME=coolstore'"
+echo "##########################################################"
 
 for LAB_NO in "$@"
 do
-    if [ $LAB_NO -eq 9 ]; then
+    if [ $LAB_NO -eq 1 ]; then
+      echo
+      echo "WARNING: Lab 1 must be done manually!"
+      echo
+    elif [ $LAB_NO -eq 9 ]; then
       echo
       echo "WARNING: Lab 9 cannot be solved using this script. It requires manual steps."
       echo
