a9bd9a72c613159fc8271b6ad125b3dd8a9c53ee
Jorge Morales <jorgemoralespou@gmail.com>
2018-11-27 16:37:45 +0100

    Update to 3.11

--- solutions/lab-5/solve.sh
+++ solutions/lab-5/solve.sh
@@ -3,7 +3,7 @@
 ###################
 
 # Deploy Web on OpenShift
-oc new-app nodejs~https://github.com/openshift-labs/cloud-native-labs.git#ocp-3.10 \
+oc new-app nodejs~https://github.com/openshift-labs/cloud-native-labs.git#ocp-3.11 \
         --context-dir=web-nodejs \
         --name=web 
 
