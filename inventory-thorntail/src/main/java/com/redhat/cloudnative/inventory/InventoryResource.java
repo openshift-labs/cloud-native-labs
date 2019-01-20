a57d37714b73d582ea1801a492feb4fdf2af0e95
mcouliba <mcouliba@redhat.com>
2019-01-20 16:41:35 +0100

    Updated to Thorntail

---
+++ inventory-thorntail/src/main/java/com/redhat/cloudnative/inventory/InventoryResource.java
@@ -0,0 +1,9 @@
+package com.redhat.cloudnative.inventory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.Path;
+
+@Path("/")
+@ApplicationScoped
+public class InventoryResource {
+}
