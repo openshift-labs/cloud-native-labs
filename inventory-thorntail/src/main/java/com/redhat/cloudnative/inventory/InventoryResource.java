3221021e41ce7b6d654b268ac35229d871ca1989
mcouliba <mcouliba@redhat.com>
2019-01-22 09:34:05 +0100

    Added beans.xml for Inventory Service

--- inventory-thorntail/src/main/java/com/redhat/cloudnative/inventory/InventoryResource.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.cloudnative.inventory;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.Path;
-
-@Path("/")
-@ApplicationScoped
-public class InventoryResource {
-}
