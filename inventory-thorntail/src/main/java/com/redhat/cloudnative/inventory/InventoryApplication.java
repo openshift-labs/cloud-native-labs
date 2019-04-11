4179637ddbb6a2cdf05e72e1c1add09ffba53828
Julien Viet <julien@julienviet.com>
2019-04-11 11:25:12 +0200

    start using vertx in Inventory

--- inventory-thorntail/src/main/java/com/redhat/cloudnative/inventory/InventoryApplication.java
+++
@@ -1,8 +0,0 @@
-package com.redhat.cloudnative.inventory;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class InventoryApplication extends Application {
-}
