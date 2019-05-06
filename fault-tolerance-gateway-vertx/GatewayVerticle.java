a76c629ecef0fde89cf06200fe7082448242bcba
Julien Viet <julien@julienviet.com>
2019-05-06 19:19:45 -0400

    Incorrect asset handler config missing *

--- fault-tolerance-gateway-vertx/GatewayVerticle.java
+++ fault-tolerance-gateway-vertx/GatewayVerticle.java
@@ -44,7 +44,7 @@ public class GatewayVerticle extends AbstractVerticle {
 
         Router router = Router.router(vertx);
         router.route().handler(CorsHandler.create("*").allowedMethod(HttpMethod.GET));
-        router.get("/").handler(StaticHandler.create("assets"));
+        router.get("/*").handler(StaticHandler.create("assets"));
         router.get("/health").handler(ctx -> ctx.response().end(new JsonObject().put("status", "UP").toString()));
         router.get("/api/products").handler(this::products);
 
